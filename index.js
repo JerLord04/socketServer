054f10fa7e7517c2afe1849fe5050b6f6933cb13
JerLord04 <h2cminute@gmail.com>
2023-01-11 22:19:14 +0700

    update roomname

--- index.js
+++ index.js
@@ -54,13 +54,16 @@ app.get('/get_humidity_data', (req, res) => {
 })
 
 app.get('/get_temperature_data', (req, res) => {
-  const sql = 'SELECT * FROM temperature_tb ORDER BY id DESC LIMIT 5';
+  const room_detail = req.query;
+  console.log(room_detail.room_id);
+  const sql = `SELECT * FROM temperature_tb WHERE room_id = ${room_detail.room_id} ORDER BY id DESC LIMIT 5;`;
   connection.query(sql, (error, results, fields) => {
     if (error) throw error;
     for (const show_data in results) {
       const converted = moment(results[show_data].date, 'YYYY-MM-DD HH:mm:ss').format('MMMM Do YYYY h:mm:ss a');
       results[show_data].date = converted;
     }
+    console.log(results);
     res.send(results);
   });
 })
